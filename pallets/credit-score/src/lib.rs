88c68b89b81bc97dc2d60e2e58dbd93e33c95b4f
juniuszhou <junius.zhou@gmail.com>
2022-01-19 15:38:20 +0800

    add credit score pallet

---
+++ pallets/credit-score/src/lib.rs
@@ -0,0 +1,42 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		CreditScoreReport(T::AccountId, u32),
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn do_something(origin: OriginFor<T>, account: T::AccountId, score: u32) -> DispatchResult {
+		
+			// TODO add the is_registered_enclave as a trait in teerex pallet. 
+			// then call it to verify the credit score report from verified TEE device.
+			let _who = ensure_signed(origin)?;
+		
+			Self::deposit_event(Event::CreditScoreReport(account, score));
+			Ok(())
+		}
+
+	}
+}
